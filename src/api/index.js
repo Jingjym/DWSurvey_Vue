385f7a230dfa3a7662c92e905c5123137964fc4e
bettermultiply <bettermultiply@gmail.com>
2023-12-25 06:58:31 -0600

    pre add the index for api and router for the convience to develop

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ export default {
   /**  账号密码登录登出  **/
   loginIn: `/api/dwsurvey/anon/security/login-pwd.do`,
   logOut: `/api/dwsurvey/anon/security/logout.do`,
+  registerAccount: `/api/dwsurvey/anon/security/register.do`, // TODO
   /** 问卷数据  **/
   surveyList: `/api/dwsurvey/app/survey/list.do`,
   surveyInfo: `/api/dwsurvey/app/survey/info.do`,
@@ -12,7 +13,9 @@ export default {
   surveyDelete: `/api/dwsurvey/app/survey/delete.do`,
   surveyReport: `/api/dwsurvey/app/stats/report.do`,
   surveyAnswerList: `/api/dwsurvey/app/answer/list.do`,
+  surveyAnsweredList: `/api/dwsurvey/app/answer/answered-list.do`, // TODO
   surveyAnswerInfo: `/api/dwsurvey/app/answer/info.do`,
+  surveyAnswerUserInfo: `/api/dwsurvey/app/answer/user-info.do`,
   surveyAnswerExport: `/api/dwsurvey/app/answer/export-xls.do`,
   surveyAnswerDelete: `/api/dwsurvey/app/answer/delete.do`,
   surveyFooterInfo: `/api/dwsurvey/anon/web/footer-info.do`,
@@ -21,5 +24,6 @@ export default {
   adminUserUpdate: `/api/dwsurvey/admin/user/edit.do`,
   adminUserDelete: `/api/dwsurvey/admin/user/delete.do`,
   curUserInfo: `/api/dwsurvey/app/user/currentUser.do`,
-  curUserPwdUpdate: `/api/dwsurvey/app/user/up-pwd.do`
+  curUserPwdUpdate: `/api/dwsurvey/app/user/up-pwd.do`,
+  curUserInfoUpdate: `/api/dwsurvey/app/user/up-info.do`
 }
